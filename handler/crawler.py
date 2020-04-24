aa2a14cc5717983a64704606627cb743e047e870
wangjunhao <wangjh@qutke.com>
2020-04-24 11:40:01 +0800

    add Logging module and encapsulate Scp impl

--- handler/crawler.py
+++ handler/crawler.py
@@ -3,18 +3,12 @@
 
 
 import tornado.web
-from spider.register import Scp
-from Utils.Utils import find_domain
+from spider.register import Sp
 
 
 class crawler_handler(tornado.web.RequestHandler):
     def get(self):
         url = self.get_argument('url')
-        domain = find_domain(url=url)
-        if url and find_domain(url=url):
-            spider = Scp.get_craw(domain=domain)()
-            spider.parser(url)
-            data = spider.get_result()
-            self.set_header('Content-type', 'application/json')
-            self.write(data)
-
+        data = Sp.parser(url=url)
+        self.set_header('Content-type', 'application/json')
+        self.write(data)
--- spider/register.py
+++ spider/register.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # coding=utf8
 from spider.baseSiteParser import BaseSiteParser
-
+from Utils.Utils import find_domain
+from Utils.logs import logger
 
 def singleton(cls):
     instance = cls()
@@ -26,3 +27,28 @@ class Scp(object):
         return None
 
 
+"""
+    封装Scp解析引擎，对外暴露解析接口
+    @:param url str
+"""
+
+
+class Sp(object):
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def parser(url=None):
+        try:
+            domain = find_domain(url=url)
+            if domain is not None:
+                spider = Scp.get_craw(domain=domain)()
+                spider.parser()
+                return spider.get_result()
+        except Exception as e:
+            logger.error('Init parsing script error {0}'.format(e))
+
+        logger.error('Cann`t find {0} script parsing engine'.format(domain))
+
+        return {}
