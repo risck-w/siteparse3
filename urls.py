5d49395c61d7020f1cefaf87d0ded576ed5b926f
wangjunhao <wangjh@qutke.com>
2020-09-30 15:19:35 +0800

    增加解析数据Rank接口

--- urls.py
+++ urls.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 # coding=utf8
 
-from handler.crawler import crawler_handler
+from handler.crawler import crawler_handler, pageRank_handler
 from handler.user import user_login_handler, users_handler
 
 
 urls = [
     (r'/craw', crawler_handler),
     (r'/user/login', user_login_handler),
-    (r'/users', users_handler)
+    (r'/users', users_handler),
+    (r'/pageRank', pageRank_handler)
 ]
